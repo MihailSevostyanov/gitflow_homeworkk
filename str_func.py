c076667ab14136a0c4e20e05b1b01a02e21c0294
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-02-20 14:56:41 +0300

    add func title and add dockstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(value):
+    """функция, делает все буквы в верхнем регистре"""
     return value.upper()
+
+
+def upper_first_letter(value):
+    """функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return value.title()
